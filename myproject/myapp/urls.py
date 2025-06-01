fce2291ac2915ff7555b98c383cb77739a15ab69
ryo <rhtky03041993@gmail.com>
2025-06-01 15:58:54 +0900

    画面遷移するように変更

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import hello_view
+from .views import hello_view, result_view
 
 urlpatterns = [
-    path('', hello_view),
+    path('', hello_view, name='hello'),
+    path('result/', result_view, name='result'),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def hello_view(request):
     return render(request, 'hello.html')
+
+def result_view(request):
+    name = request.GET.get('name')  # フォームからのGETデータを取得
+    return render(request, 'result.html', {'name': name})
