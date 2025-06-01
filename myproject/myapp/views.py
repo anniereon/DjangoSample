17f44660d504c1c331bd2a944d6ce3c993c3f906
ryo <rhtky03041993@gmail.com>
2025-06-01 16:10:21 +0900

    モデルを使うよう変更

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import Name
 
 def hello_view(request):
     return render(request, 'hello.html')
 
 def result_view(request):
-    name = request.GET.get('name')  # フォームからのGETデータを取得
-    return render(request, 'result.html', {'name': name})
+    name_text = request.GET.get('name')  # ← GETで取得
+    if name_text:
+        name_obj = Name.objects.create(text=name_text)  # ← データベースに保存
+        return render(request, 'result.html', {'name': name_obj.text})
+    else:
+        return render(request, 'result.html', {'name': '名前が指定されていません'})
