672c4831ac3a327d608c074fa49c8e7187609bda
ryo <rhtky03041993@gmail.com>
2025-06-25 02:17:39 +0900

    テーブルから取得したユーザ名を出力してCSVファイル成するよう変更

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,9 @@
+import csv
+import os
+
 from django.shortcuts import render
 from .models import User
+from django.conf import settings
 
 def hello_view(request):
     return render(request, 'hello.html')
@@ -12,4 +16,15 @@ def result_view(request):
 
     # 全ユーザー取得
     users = User.objects.all()
+
+    # CSVファイルへの書き込み
+    csv_file_path = os.path.join(settings.BASE_DIR, 'users.csv')
+    with open(csv_file_path, mode='w', newline='', encoding='cp932') as csvfile:
+        writer = csv.writer(csvfile)
+        # ヘッダー行（必要に応じてフィールド名を追加）
+        writer.writerow(['ユーザー名'])
+        # データ行
+        for user in users:
+            writer.writerow([user.user_name])
+
     return render(request, 'result.html', {'users': users})
